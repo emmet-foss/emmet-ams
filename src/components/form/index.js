aac93e3654b6ebdfce8731bff19c7651788c5c62
East <eastmaels@gmail.com>
2019-07-30 18:58:57 +0900

    return form/index.js

---
+++ src/components/form/index.js
@@ -0,0 +1,3 @@
+export { default as AttendanceConfirm } from './AttendanceConfirm';
+export { default as AttendanceForm } from './AttendanceForm';
+export { default as ReportForm } from './ReportForm';
