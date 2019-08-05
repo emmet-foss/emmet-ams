b0135d0894055949162f832b62eeb2f82a759d99
East <eastmaels@gmail.com>
2019-08-05 19:42:56 +0900

    add loading state to attendance list

--- src/components/list/AttendanceList.js
+++ src/components/list/AttendanceList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import {
-  Button, Col, Icon, Row, Statistic, Table,
+  Button, Col, Icon, Row, Spin, Statistic, Table,
 } from 'antd';
 import * as qs from 'query-string';
 import ReactGA from 'react-ga';
@@ -36,18 +36,20 @@ class AttendanceList extends Component {
     result: [],
     period: "",
     localeInfo: "",
+    loadingAttendance: false,
+    loadingLocaleInfo: false,
   };
 
   componentDidMount() {
     this.getMonthlyAttendance()
       .then(res => {
-        this.setState({ result: res.result })
+        this.setState({ result: res.result, loadingAttendance: false })
       })
       .catch(err => console.log(err));  
 
     this.getLocaleInfo()
       .then(res => {
-        this.setState({ localeInfo: res.locale })
+        this.setState({ localeInfo: res.locale, loadingLocaleInfo: false })
       })
       .catch(err => console.log(err));  
 
@@ -59,13 +61,13 @@ class AttendanceList extends Component {
     if (nextProps.location !== this.props.location) {
       this.getMonthlyAttendance()
         .then(res => {
-          this.setState({ result: res.result })
+          this.setState({ result: res.result, loadingAttendance: false })
         })
         .catch(err => console.log(err));
 
       this.getLocaleInfo()
         .then(res => {
-          this.setState({ localeInfo: res.locale })
+          this.setState({ localeInfo: res.locale, loadingLocaleInfo: false })
         })
         .catch(err => console.log(err));  
   
@@ -77,6 +79,7 @@ class AttendanceList extends Component {
   getMonthlyAttendance = async () => {
     const localeId = this.props.location.pathname.split('/')[2];
     const query = qs.parse(this.props.location.search);
+    this.setState({ loadingAttendance: true })
     const response = await emmetAPI.getUrl(`/ams/attendance/by_date?localeId=${localeId}&attendanceDate=${query.attendanceDate}`)
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
@@ -85,6 +88,7 @@ class AttendanceList extends Component {
 
   getLocaleInfo = async () => {
     const localeId = this.props.location.pathname.split('/')[2];
+    this.setState({ loadingLocaleInfo: true });
     const response = await emmetAPI.getUrl(`/ams/locale_churches/${localeId}`)
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
@@ -105,54 +109,64 @@ class AttendanceList extends Component {
   render() {
     const query = qs.parse(this.props.location.search);
     const { attendanceDate } = query;
-    const { result, localeInfo } = this.state;
+    const { result, localeInfo, loadingLocaleInfo, loadingAttendance } = this.state;
+    const loading = (loadingLocaleInfo || loadingAttendance );
+
     return (
       <div className="wrap">
         <div className="extraContent">
-          <Row type="flex" justify="center">
-            <Col xs={24} sm={24} md={24} lg={12}>
-            {(result && result.length === 0) ?
-              <div>
-                <Statistic value={`No ${localeInfo.name} attendance available for ${attendanceDate}.`} />
-                <Statistic value={"Would you like to submit an attendance?"} suffix={
-                  <span>
-                    <NavLink to={`/locale_church/${localeInfo._id}/attendance?attendanceDate=${attendanceDate}`}>
-                      <Button type="primary" size="small">
-                        <Icon type="check"/>Yes
-                      </Button>
-                    </NavLink>
-                    <NavLink to={`/locale_church/${localeInfo._id}/calendar_form`}>
-                      <Button type="primary" size="small">
-                        <Icon type="cross"/>No
-                      </Button>
-                    </NavLink>
-                  </span>
-                }/>
-              </div>
-            :
-              <div>
-                <Statistic value={"Here's the attendance for"} />
-                <Statistic value={`${localeInfo.name}, ${attendanceDate}:`} />
-                <Table pagination={false} columns={columns} dataSource={result} />
+          {loading ?
+            <Row type="flex" justify="center">
+              <Col xs={24} sm={24} md={24} lg={12} style={{ textAlign: "center" }}>
+                <Spin size="large" />
+              </Col>
+            </Row>
+          :
+            <Row type="flex" justify="center">
+              <Col xs={24} sm={24} md={24} lg={12}>
+              {(result && result.length === 0) ?
+                <div>
+                  <Statistic value={`No ${localeInfo.name} attendance available for ${attendanceDate}.`} />
+                  <Statistic value={"Would you like to submit an attendance?"} suffix={
+                    <span>
+                      <NavLink to={`/locale_church/${localeInfo._id}/attendance?attendanceDate=${attendanceDate}`}>
+                        <Button type="primary" size="small">
+                          <Icon type="check"/>Yes
+                        </Button>
+                      </NavLink>
+                      <NavLink to={`/locale_church/${localeInfo._id}/calendar_form`}>
+                        <Button type="primary" size="small">
+                          <Icon type="cross"/>No
+                        </Button>
+                      </NavLink>
+                    </span>
+                  }/>
+                </div>
+              :
+                <div>
+                  <Statistic value={"Here's the attendance for"} />
+                  <Statistic value={`${localeInfo.name}, ${attendanceDate}:`} />
+                  <Table pagination={false} columns={columns} dataSource={result} />
 
-                <Statistic value={"Would you like to submit another?"} suffix={
-                  <span>
-                    <NavLink to={`/locale_church/${localeInfo._id}/attendance?attendanceDate=${attendanceDate}`}>
-                      <Button type="primary" size="small">
-                        <Icon type="check"/>Yes
-                      </Button>
-                    </NavLink>
-                    <NavLink to={`/locale_church/${localeInfo._id}/calendar_form`}>
-                      <Button type="primary" size="small">
-                        <Icon type="cross"/>No
-                      </Button>
-                    </NavLink>
-                  </span>
-                } />
-              </div>
-            }
-            </Col>
-          </Row>
+                  <Statistic value={"Would you like to submit another?"} suffix={
+                    <span>
+                      <NavLink to={`/locale_church/${localeInfo._id}/attendance?attendanceDate=${attendanceDate}`}>
+                        <Button type="primary" size="small">
+                          <Icon type="check"/>Yes
+                        </Button>
+                      </NavLink>
+                      <NavLink to={`/locale_church/${localeInfo._id}/calendar_form`}>
+                        <Button type="primary" size="small">
+                          <Icon type="cross"/>No
+                        </Button>
+                      </NavLink>
+                    </span>
+                  } />
+                </div>
+              }
+              </Col>
+            </Row>
+          }
         </div>
       </div>
     );
